b4db123e9ac444bc4cc31a6783681b0f9589b74a
gayathma <gayathma3@gmail.com>
2014-06-24 20:56:40 +0530

    db pas changed

--- ewcfg10.php
+++ ewcfg10.php
@@ -46,7 +46,7 @@ $EW_FONT_PATH = realpath('./phpfont');
 define("EW_CONN_HOST", 'localhost', TRUE);
 define("EW_CONN_PORT", 3306, TRUE);
 define("EW_CONN_USER", 'root', TRUE);
-define("EW_CONN_PASS", 'connectit1', TRUE);
+define("EW_CONN_PASS", 'gaya', TRUE);
 define("EW_CONN_DB", 'db_msc_online_exams', TRUE);
 
 // ADODB (Access/SQL Server)
