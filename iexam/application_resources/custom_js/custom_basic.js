54b4a25d3a38b63bde01d0ef9552df866f759863
gayathma <gayathma3@gmail.com>
2014-07-02 21:08:02 +0530

    fdf

--- iexam/application_resources/custom_js/custom_basic.js
+++ iexam/application_resources/custom_js/custom_basic.js
@@ -384,6 +384,59 @@ function delete_maritalstatus(id) {
 }
 
 
+////////////////////////////Log In////////////////////////////
+
+function LoginForm() {
+    $('#login_form').bootstrapValidator({
+        message: 'This value is not valid',
+        fields: {
+            txtusername: {
+                validators: {
+                    notEmpty: {
+                        message: 'This feild is required \n'
+                    },
+                    stringLength: {
+                        min: 6,
+                        message: 'The username must be more than 6 characters long'
+                    }
+                }
+            },
+            txtpassword: {
+                validators: {
+                    notEmpty: {
+                        message: 'This feild is required \n'
+                    }
+                }
+            }
+
+        }, submitHandler: function(form) {
+            var login_username = $('#txtusername').val();
+            var login_password = $('#txtpassword').val();
+
+
+//        var x = $('.load-anim').show().delay(5000);
+
+            $.ajax({
+                type: "POST",
+                url: site_url + "/login/login_controller/authenticate_user",
+                data: "login_username=" + login_username + "&login_password=" + login_password,
+                async: false,
+                success: function(msg) {
+                    $('#msg').html('<span class="response-msg notice ui-corner-all">validating...</span>');
+                    if (msg == 1) {
+                        $('#msg').html('<span class="response-msg notice ui-corner-all">validating...</span>');
+                        setTimeout("location.href = site_url+'/login/login_controller/';", 100);
+                        x.fadeOut('slow');
+                    } else {
+
+                        $('#msg').html('<div class="alert alert-error"><button class="close" data-dismiss="alert"></button>Invalid login details...</div>');
+                    }
+
+                }
+            });
+        }
+    });
+}
 
 
 
